f9e1b4c9afd2be3f63a1e469ea35dd0707a7ca0d
Evgueni <shakhtarine.evgueni@gmail.com>
2016-09-27 13:49:52 +0200

    Merge remote-tracking branch 'origin/master' into planning
    # Conflicts:
    #	controllers/Class.Controller.php
    #	views/header.inc
    #	views/sorties/sorties.php

--- controllers/Class.Controller.php
+++ controllers/Class.Controller.php
@@ -4,48 +4,53 @@
  * @author S. Martin
  * @link http://www.hevs.ch	
  */
-class Controller {	
+class Controller {
     protected $vars = array();
     protected $controller;
     protected $method;
-   
+
     /**
      * Constructor
      * @param string $controller
      * @param string $method
      */
     function Controller($controller, $method) {
+        $this->vars['pageTitle'] = "CAS";
+        $this->vars['pageMessage'] = "Club Alpin Suisse";
         $this->controller = $controller;
-        $this->method = $method;        
+        $this->method = $method;
+
     }
-    
+
     /**
      * Display view associated to a controller method
      */
     function display(){
-    	$view = "{$this->controller}/{$this->method}.php";
-    	if(file_exists('views/'.$view))
-			include 'views/'.$view;
+
+        $view = "{$this->controller}/{$this->method}.php";
+
+        if(file_exists('views/'.$view))
+            include 'views/'.$view;
     }
-    
+
     /**
      * URL redirection
      * @param string $controller
-     * @param string $method    
+     * @param string $method
      */
-    function redirect($controller, $method) {    	
-    	$url = "Location: " . URL_DIR. $controller . '/' .$method;    	
-    	header($url);
+    function redirect($controller, $method) {
+        $url = "Location: " . URL_DIR. $controller . '/' .$method;
+        header($url);
     }
-    
+
     /**
      * Get active (logged-in) user
      * @return User
      */
     function getActiveUser(){
-    	if(isset($_SESSION['user']))
-    		return $_SESSION['user'];
-    	else
-    		return false;
+        if(isset($_SESSION['user']))
+            return $_SESSION['user'];
+        else
+            return false;
     }
 }
