7c969571cb68c6f704b34bb205f316d65e9a90b1
andreas <andreasfi@me.com>
2016-09-29 09:04:41 +0200

    Remove .idea from repo

--- controllers/Class.sortiesController.php
+++ controllers/Class.sortiesController.php
@@ -18,10 +18,12 @@ class sortiesController extends Controller{
     }
     function details(){
         // Get infos
+        $result = Event::fetch_all_events();
+        // Get infos
 
         $result = Event::fetch_all_events();
         // Variables depuis BD
-        
+
         $description = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
 
         $this->vars['title'] = $result[0]->getTitle();
@@ -52,10 +54,10 @@ class sortiesController extends Controller{
 		$this->vars['msg'] = isset($_SESSION['msg']) ? $_SESSION['msg'] : '';
         $this->vars['pageTitle'] = "Ajouter une course";
         $this->vars['pageMessage'] = "";
-			
-			
+
+
 		if(!empty($_POST)){
-			
+
 			$description = $_POST['description'];
 			$start_date = $_POST['startDate'].' '.$_POST['startTime'].':00';
 			$end_date = $_POST['endDate'].' '.$_POST['endTime'].':00';
@@ -66,7 +68,7 @@ class sortiesController extends Controller{
 			$event_cat = $_POST['category'];
 			$difficulty = $_POST['difficulty'];
 			$path = $_POST['JSON'];
-			
+
 			$event = new Event($id = null, $description, $start_date, $end_date, $max_participants, $event_type, $owner, $title, $event_cat, $difficulty, $path);
 			$event->save();
 		}
