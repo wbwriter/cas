b73be60ede08b3d3050ac6afb2b8024ffa61d25c
andreas <andreasfi@me.com>
2016-10-03 10:45:01 +0200

    details sorties

--- controllers/Class.sortiesController.php
+++ controllers/Class.sortiesController.php
@@ -50,26 +50,27 @@ class sortiesController extends Controller{
 
     function ajoutsortie()
     {
-		$this->vars['msg'] = isset($_SESSION['msg']) ? $_SESSION['msg'] : '';
+        $this->vars['msg'] = isset($_SESSION['msg']) ? $_SESSION['msg'] : '';
         $this->vars['pageTitle'] = "Ajouter une course";
         $this->vars['pageMessage'] = "";
 
 
-		if(!empty($_POST)){
+        if (!empty($_POST)) {
 
-			$description = $_POST['description'];
-			$start_date = $_POST['startDate'].' '.$_POST['startTime'].':00';
-			$end_date = $_POST['endDate'].' '.$_POST['endTime'].':00';
-			$max_participants = $_POST['maxParticipants'];
-			$event_type = 1;
-			$owner = 1;//$_SESSION['user']->getId();
-			$title = $_POST['title'];
-			$event_cat = $_POST['category'];
-			$difficulty = $_POST['difficulty'];
-			$path = $_POST['JSON'];
+            $description = $_POST['description'];
+            $start_date = $_POST['startDate'] . ' ' . $_POST['startTime'] . ':00';
+            $end_date = $_POST['endDate'] . ' ' . $_POST['endTime'] . ':00';
+            $max_participants = $_POST['maxParticipants'];
+            $event_type = 1;
+            $owner = 1;//$_SESSION['user']->getId();
+            $title = $_POST['title'];
+            $event_cat = $_POST['category'];
+            $difficulty = $_POST['difficulty'];
+            $path = $_POST['JSON'];
 
-			$event = new Event($id = null, $description, $start_date, $end_date, $max_participants, $event_type, $owner, $title, $event_cat, $difficulty, $path);
-			$event->save();
-		}
+            $event = new Event($id = null, $description, $start_date, $end_date, $max_participants, $event_type, $owner, $title, $event_cat, $difficulty, $path);
+            $event->save();
+        }
+    }
 
 }
